a15fe786b75e21bc55d010dba1352229b39dda20
DavidJ <davidjakes2@gmail.com>
2018-04-09 19:06:05 +0100

    Copyright notices

--- src/main/java/analysis/graph/GraphUtilities.java
+++ src/main/java/analysis/graph/GraphUtilities.java
@@ -1,3 +1,6 @@
+// Copyright 2018 David Jakes.
+// This program is distributed under the terms of the GNU General Public License.
+
 package analysis.graph;
 
 import java.math.BigDecimal;
@@ -9,6 +12,7 @@ public class GraphUtilities
 {
 	/**
 	 * Calculates the Euclidean distance of the segment represented by point1 and point2.
+	 * 
 	 * @return BigDecimal representing this segment's Euclidean distance.
 	 */
 	public static BigDecimal calculateEuclideanDistance(Point point1, Point point2)
@@ -17,12 +21,12 @@ public class GraphUtilities
 		double y1 = point1.getY().doubleValue();
 		double x2 = point2.getX().doubleValue();
 		double y2 = point2.getY().doubleValue();
-		
+
 		double euclideanDistance = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
-		
+
 		return new BigDecimal(euclideanDistance);
 	}
-	
+
 	private GraphUtilities()
 	{
 
