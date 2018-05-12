aa05a6b7fdc888d5e23f4c8eabb61b3632ea55aa
DavidJ <davidjakes2@gmail.com>
2018-05-12 15:34:04 +0100

    Four maximum constraints implemented

--- src/main/java/analysis/linguistics/contentdetermination/concepts/MaximumAssessor.java
+++ src/main/java/analysis/linguistics/contentdetermination/concepts/MaximumAssessor.java
@@ -23,6 +23,7 @@ import analysis.constrain.SoftConstraintGroup;
 import analysis.constrain.WeightedAverageConstraintProcessor;
 import analysis.graph.Point;
 import analysis.interfaces.Assessor;
+import analysis.statistics.Maximum;
 import analysis.statistics.NinetyPercentile;
 import analysis.statistics.UpperTurningPointsSmoothed;
 import writenlg.control.WriteNlgProperties;
@@ -60,6 +61,107 @@ public class MaximumAssessor implements Assessor
 	public void assessConstraints()
 	{
 		assessSingleObviousMaximumConstraint();
+		assessMultipleHighTurningPointsConstraint();
+		assessMaximumAtStartConstraint();
+		assessMaximumAtEndConstraint();
+	}
+
+	private void assessMaximumAtStartConstraint()
+	{
+		final ConstraintConfiguration maximumAtStartConstraintConfiguration = this.constraints
+				.get(ConstraintType.MAXIMUM_AT_START.getTextualForm());
+
+		Maximum maximum = new Maximum(this.timeSeries.getTimeSeries());
+		Point maximumPoint = maximum.calculate();
+
+		BigDecimal maximumAtStartConstraintValue = null;
+
+		if (maximumPoint.getX().compareTo(new BigDecimal("0")) == 0)
+		{
+			maximumAtStartConstraintValue = new BigDecimal("1")
+					.multiply(maximumAtStartConstraintConfiguration.getValue());
+		}
+		else
+		{
+			maximumAtStartConstraintValue = new BigDecimal("0")
+					.multiply(maximumAtStartConstraintConfiguration.getValue());
+		}
+
+		final Constraint<ConstraintType> maximumAtStartConstraint = new BoundedWeightedConstraint<ConstraintType>(
+				ConstraintType.MAXIMUM_AT_START,
+				new SatisfactionLevel(maximumAtStartConstraintValue,
+						maximumAtStartConstraintConfiguration.getWeighting()),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintLowerBound")),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintUpperBound")));
+
+		this.constraintTypes.addConstraint(maximumAtStartConstraint);
+	}
+
+	private void assessMaximumAtEndConstraint()
+	{
+		final ConstraintConfiguration maximumAtEndConstraintConfiguration = this.constraints
+				.get(ConstraintType.MAXIMUM_AT_END.getTextualForm());
+
+		Maximum maximum = new Maximum(this.timeSeries.getTimeSeries());
+		Point maximumPoint = maximum.calculate();
+
+		BigDecimal maximumAtEndConstraintValue = null;
+
+		if (maximumPoint.getX().compareTo(new BigDecimal(this.timeSeries.getPoints().size() - 1)) == 0)
+		{
+			maximumAtEndConstraintValue = new BigDecimal("1").multiply(maximumAtEndConstraintConfiguration.getValue());
+		}
+		else
+		{
+			maximumAtEndConstraintValue = new BigDecimal("0").multiply(maximumAtEndConstraintConfiguration.getValue());
+		}
+
+		final Constraint<ConstraintType> maximumAtEndConstraint = new BoundedWeightedConstraint<ConstraintType>(
+				ConstraintType.MAXIMUM_AT_END,
+				new SatisfactionLevel(maximumAtEndConstraintValue, maximumAtEndConstraintConfiguration.getWeighting()),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintLowerBound")),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintUpperBound")));
+
+		this.constraintTypes.addConstraint(maximumAtEndConstraint);
+	}
+
+	private void assessMultipleHighTurningPointsConstraint()
+	{
+		final ConstraintConfiguration multipleHighTurningPointsConstraintConfiguration = this.constraints
+				.get(ConstraintType.MULTIPLE_HIGH_TURNING_POINTS.getTextualForm());
+
+		int highPointCount = calculateHighPointsCount();
+
+		BigDecimal multipleHighTurningPointsConstraintValue = null;
+
+		switch (highPointCount)
+		{
+			case 1:
+				multipleHighTurningPointsConstraintValue = new BigDecimal("0")
+						.multiply(multipleHighTurningPointsConstraintConfiguration.getValue());
+				break;
+			case 2:
+				multipleHighTurningPointsConstraintValue = new BigDecimal("0.3")
+						.multiply(multipleHighTurningPointsConstraintConfiguration.getValue());
+				break;
+			case 3:
+				multipleHighTurningPointsConstraintValue = new BigDecimal("0.6")
+						.multiply(multipleHighTurningPointsConstraintConfiguration.getValue());
+				break;
+			default:
+				multipleHighTurningPointsConstraintValue = new BigDecimal("1")
+						.multiply(multipleHighTurningPointsConstraintConfiguration.getValue());
+				break;
+		}
+
+		final Constraint<ConstraintType> multipleHighTurningPointsConstraint = new BoundedWeightedConstraint<ConstraintType>(
+				ConstraintType.MULTIPLE_HIGH_TURNING_POINTS,
+				new SatisfactionLevel(multipleHighTurningPointsConstraintValue,
+						multipleHighTurningPointsConstraintConfiguration.getWeighting()),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintLowerBound")),
+				new BigDecimal(WriteNlgProperties.getInstance().getProperty("WeightedConstraintUpperBound")));
+
+		this.constraintTypes.addConstraint(multipleHighTurningPointsConstraint);
 	}
 
 	private void assessSingleObviousMaximumConstraint()
@@ -67,30 +169,7 @@ public class MaximumAssessor implements Assessor
 		final ConstraintConfiguration singleObviousMaximumConstraintConfiguration = this.constraints
 				.get(ConstraintType.SINGLE_OBVIOUS_MAXIMUM.getTextualForm());
 
-		UpperTurningPointsSmoothed upperTurningPoints = new UpperTurningPointsSmoothed(this.timeSeries);
-		List<Point> ninetyPercentileSmoothed = upperTurningPoints.identify();
-
-		int highPointCount = ninetyPercentileSmoothed.size();
-
-		NinetyPercentile ninetyPercentile = new NinetyPercentile(this.timeSeries);
-		Point[] highPoints = ninetyPercentile.identify();
-
-		for (Point eachPoint : highPoints)
-		{
-			if (eachPoint.getX().compareTo(this.timeSeries.getPoints().get(0).getX()) == 0)
-			{
-				highPointCount++;
-			}
-
-			if (eachPoint.getX()
-					.compareTo(this.timeSeries.getPoints().get(this.timeSeries.getPoints().size() - 1).getX()) == 0)
-			{
-				highPointCount++;
-			}
-		}
-
-		LOGGER.info(String.format("Count of high points, including maximum: %d", highPointCount));
-
+		int highPointCount = calculateHighPointsCount();
 		BigDecimal singleObviousMaximumConstraintValue = null;
 
 		switch (highPointCount)
@@ -129,5 +208,34 @@ public class MaximumAssessor implements Assessor
 	public ConstraintGroup<ConstraintType> getMaximumConstraints()
 	{
 		return this.constraintTypes;
+	}
+
+	private int calculateHighPointsCount()
+	{
+		UpperTurningPointsSmoothed upperTurningPoints = new UpperTurningPointsSmoothed(this.timeSeries);
+		List<Point> ninetyPercentileSmoothed = upperTurningPoints.identify();
+
+		int highPointCount = ninetyPercentileSmoothed.size();
+
+		NinetyPercentile ninetyPercentile = new NinetyPercentile(this.timeSeries);
+		Point[] highPoints = ninetyPercentile.identify();
+
+		for (Point eachPoint : highPoints)
+		{
+			if (eachPoint.getX().compareTo(this.timeSeries.getPoints().get(0).getX()) == 0)
+			{
+				highPointCount++;
+			}
+
+			if (eachPoint.getX()
+					.compareTo(this.timeSeries.getPoints().get(this.timeSeries.getPoints().size() - 1).getX()) == 0)
+			{
+				highPointCount++;
+			}
+		}
+
+		LOGGER.info(String.format("Count of high points, including maximum: %d", highPointCount));
+
+		return highPointCount;
 	}
 }
